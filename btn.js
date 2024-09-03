132e8a1af1ba815d45dede2df7363196e38e1941
AnibalSlv2 <anibalsilva19@gmail.com>
2024-09-03 14:42:24 -0400

    Add files via upload

--- btn.js
+++ btn.js
@@ -1,15 +1,7 @@
-var btnRotate = document.querySelector(".btnRotate")
-
 document.querySelector(".btn").addEventListener("click", function(){
     document.querySelector(".btnContent").classList.remove("btnContentNone");
 })
 
 document.querySelector(".btn").addEventListener("mouseout", function(){
     document.querySelector(".btnContent").classList.add("btnContentNone");
-    btnRotate.classList.remove("btnRotateNone");
-})
-
-
-document.querySelector(".btnRotate").addEventListener("mouseover", function(){
-    btnRotate.classList.add("btnRotateNone");
 })
